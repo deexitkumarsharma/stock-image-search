f8a315f91ac56218333fdbfdf06ca6f1cc4858b0
Deexit Kumar Sharma <deexitkumarsharma@gmail.com>
2022-12-20 12:22:19 +0530

    Refactored the code for slow fetching issue

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-const input = document.getElementById("input");
-const grid = document.getElementsByClassName("grid")[0];
+dayNightTheme = () => {
+  let date = new Date();
+  let hour = date.getHours();
 
-window.addEventListener("load", dayNightMode);
-
-input.addEventListener("keydown", function (event) {
-  if (event.key === "Enter") {
-    loadImg();
+  if (hour >= 7 && hour < 19) {
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+  } else {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
   }
+};
+
+window.addEventListener("load", dayNightTheme);
+
+document.querySelector("#input").addEventListener("keydown", (event) => {
+  if (event.key == "Enter") apiRequest();
 });
 
-function loadImg() {
-  removeImage();
+apiRequest = () => {
+  document.querySelector("#grid").textContent = "";
 
   const url =
     "https://api.unsplash.com/search/photos?query=" +
@@ -19,39 +27,26 @@ function loadImg() {
 
   fetch(url)
     .then((response) => {
-      // console.log(response);
       if (!response.ok) throw Error(response.statusText);
       return response.json();
     })
 
     .then((data) => {
-      const imageNodes = [];
-      for (let i = 0; i < data.results.length; i++) {
-        imageNodes[i] = document.createElement("div");
-        imageNodes[i].className = "img";
-        imageNodes[i].style.backgroundImage =
-          "url(" + data.results[i].urls.raw + ")";
-        imageNodes[i].addEventListener("dblclick", function () {
-          window.open(data.results[i].links.download, "_blank");
-        });
-        grid.appendChild(imageNodes[i]);
-      }
+      loadImages(data);
+    })
+
+    .catch((error) => console.log(error));
+};
+
+loadImages = (data) => {
+  for (let i = 0; i < data.results.length; i++) {
+    let image = document.createElement("div");
+    image.className = "img";
+    image.style.backgroundImage =
+      "url(" + data.results[i].urls.raw + "&w=1366&h=768" + ")";
+    image.addEventListener("dblclick", function () {
+      window.open(data.results[i].links.download, "_blank");
     });
-}
-
-function removeImage() {
-  grid.innerHTMl = "";
-}
-
-function dayNightMode() {
-  const date = new Date();
-  const hour = date.getHours();
-
-  if (hour >= 7 && hour <= 19) {
-    document.body.style.backgroundColor = "white";
-    document.body.style.color = "black";
-  } else {
-    document.body.style.backgroundColor = "black";
-    document.body.style.color = "white";
+    document.querySelector("#grid").appendChild(image);
   }
-}
+};
